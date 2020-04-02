3ffd87c74c7bf26eda50822b4ef915e3ce687e8a
node.dom <dominique.reese444@gmail.com>
2020-04-02 18:53:44 -0400

    Implementing dot-env pkg

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { TorrentModule } from "./modules";
-
+require("dotenv").config();
 const movieController1 = TorrentModule.MovieController;
+const CURRENT_ENVIRONMENT = process.env.ENVIRONMENT;
 
+console.log(`Current environment: [ ${CURRENT_ENVIRONMENT} ] `);
 movieController1
   .getMovieInTopQuality("")
   .then((movies) => {
